64b65a96d07dd0b9a8ddb96a5abe1cf320265fdb
alexanderlonvii <alexanderleonvii@gmail.com>
2019-04-14 14:02:34 -0500

    Agregando Google fonts

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello world2</h1>
+        <h1>Unicornios</h1>
         <hr/>
         <Link to="/home">Home</Link>
         <Link to="/about">About</Link>
