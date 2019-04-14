98ac365f1bef68b1252b49f87f9fb9f22b3945a3
alexanderlonvii <alexanderleonvii@gmail.com>
2019-04-14 14:30:36 -0500

    Agregando el componente header

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,10 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
-import Home from './Home';
-import About from './About';
-import View from './View';
+import Header from './Header'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Unicornios</h1>
-        <hr/>
-        <Link to="/home">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/view/1">View</Link>
-        <Link to="/view/2">View</Link>
-        <hr/>
-        <Route path="/home" component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/view/:id" component={View}/>
-      </div>
+      <Header/>
     );
   }
 }
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import View from './View';
+
+const Header = (props) => {
+  return(
+    <div>
+      <h1>Unicornios</h1>
+      <hr/>
+      <Link to="/home">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/view/1">View</Link>
+      <Link to="/view/2">View</Link>
+      <hr/>
+      <Route path="/home" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/view/:id" component={View}/>
+    </div>
+  )
+}
+
+export default Header
