970aded408dc0cff977e953cf38656041f276912
alexanderlonvii <alexanderleonvii@gmail.com>
2019-04-14 19:03:30 -0500

    Agregando otro ejemplo y estilos basicos en los card Item List

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,13 +3,21 @@ import CardListItem from './CardListItem'
 
 import gaticornio from '../Images/gaticornio.jpg'
 import gaticorniotxt from '../Legends/gaticornio.txt'
+import ornitorrincornio from '../Images/ornitorrincornio.jpg'
+import ornitorrincorniotxt from '../Legends/gaticornio.txt'
 
 const CardList = (props) =>{
   return(
     <div>
       <CardListItem
+        unicorn='Gaticornio'
         image={gaticornio}
         text={gaticorniotxt}
+      />
+      <CardListItem
+        unicorn='Ornitorrincornio'
+        image={ornitorrincornio}
+        text={ornitorrincorniotxt}
       />
     </div>
   )
--- src/components/CardListItem.js
+++ src/components/CardListItem.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const CardListItem = (props) => {
-  let {image, text} = props
+  let {image, text, unicorn} = props
   return(
     <div class="cardItem">
+      <h2>{unicorn}</h2>
       <figure>
         <img src={image}/>
       </figure>
